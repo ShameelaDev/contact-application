3351bef7bdd4eecccaefdc8b2a37df7a3b628277
ShameelaDev <shemeelamoidheen@gmail.com>
2022-03-19 13:05:39 +0530

    project created

---
+++ lib/src/bloc/contact/contact_state.dart
@@ -0,0 +1,25 @@
+part of 'contact_cubit.dart';
+
+abstract class ContactState extends Equatable {
+  const ContactState();
+}
+
+class ContactInitial extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+class ContactLoading extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+class ContactLoaded extends ContactState {
+  @override
+  List<Object> get props => [];
+}
+
+class ContactLoadError extends ContactState {
+  @override
+  List<Object> get props => [];
+}
